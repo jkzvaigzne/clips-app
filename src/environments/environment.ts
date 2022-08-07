a05fd326b493c93ec7670020b4498524a8377da0
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2022-08-07 18:54:49 +0300

    Completed Project Clips

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD3BkZnZaRXQP4ozxollSghtOnuSPDf-fE",
+    authDomain: "clips-9f03c.firebaseapp.com",
+    projectId: "clips-9f03c",
+    storageBucket: "clips-9f03c.appspot.com",
+    appId: "1:820895541715:web:294aa34f6252e3570da726"
+  }
 };
 
 /*
